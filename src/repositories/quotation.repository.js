eb06bf065081bad2de21539bcf58854fd03ce2b1
Edgar Franco <leivachavezedgarfranco@gmail.com>
2026-06-11 12:43:53 -0500

    add QuotationRepository for managing quotations and purchase order file URLs

---
+++ src/repositories/quotation.repository.js
@@ -0,0 +1,27 @@
+const db = require('../config/db');
+const deleteFile = require('../utils/deleteFile');
+
+const QuotationRepository = {
+    async getQuotationByID(quotationID) {
+        const quotationRows = await db.query(`
+            SELECT * 
+            FROM COTIZACION_COMERCIAL cc 
+            WHERE cc.ID = (?)
+        `, [quotationID])
+        return quotationRows[0]
+    },
+
+    async upsertPurchaseOrderFileURL(filename, QuotationID) {
+        // Guardar URL relativa en la base de datos (ej: /uploads/cotizaciones/orden_compra_xxx.pdf)
+        const RelativePurchaseOrderFileUrl = `/uploads/cotizaciones/${filename}`;
+        await db.query(`
+            UPDATE COTIZACION_COMERCIAL 
+            SET Orden_compra = (?) 
+            WHERE ID = (?)
+        `, [RelativePurchaseOrderFileUrl, QuotationID]);
+
+        return RelativePurchaseOrderFileUrl;
+    }
+};
+
+module.exports = QuotationRepository;
